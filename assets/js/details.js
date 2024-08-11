df178834ae9d38e471cd78027bcf7f3e9c257a76
Jorge <jorgelorduy4@outlook.com>
2024-08-11 02:49:24 -0500

    casi listo

--- assets/js/details.js
+++ assets/js/details.js
@@ -195,93 +195,9 @@ const data = {
   ],
 };
 
-let contenedor = document.getElementById("contenedor");
-function crearEventos(a) {
-  contenedor.innerHTML = ``;
-  for (let i = 0; i < a.length; i++) {
-    let fecha = data.events[i].date.slice(0, 4);
-    parseInt(fecha);
-    let hoy = data.currentDate.slice(0, 4);
-    parseInt(hoy);
-    if (fecha >= hoy) {
-      let evento = document.createElement("div");
-      evento.classList.add("card", "p-3");
-      evento.innerHTML = `<img src=${a[i].image} class="card-img-top" alt="...">
-  <div class="card-body d-flex flex-column justify-content-between">
-    <h5 class="card-title">${a[i].name}</h5>
-    <p class="card-text">${a[i].description}</p>
-    <div><p class="fw-bold fs-5">Price: $${a[i].price}</p>
-    <a href="./details.html?id=${a[i]._id}" class="btn btn-primary">Details</a></div>
-  </div>`;
+let urlObjeto = new URLSearchParams(window.location.search);
+let identificador = urlObjeto.get("id");
 
-      contenedor.appendChild(evento);
-    }
-  }
-}
-crearEventos(data.events);
+let eventoRender = data.events.find((i) => i._id == identificador);
 
-function crearCheckbox() {
-  let checkboxcreate = [];
-  for (let i = 0; i < data.events.length; i++) {
-    let actual = data.events[i].category;
-    if (checkboxcreate.includes(actual)) {
-    } else {
-      checkboxcreate.push(actual);
-    }
-  }
-  let lugar = document.getElementById("checkBoxHome");
-  let contador = 0;
-  for (let i = 0; i < checkboxcreate.length; i++) {
-    let label = document.createElement("label");
-    contador++;
-    label.innerHTML = `<input type="checkBox" name="option" value="${checkboxcreate[i]}">${checkboxcreate[i]}`;
-    lugar.appendChild(label);
-  }
-}
-crearCheckbox();
-let filtros = document.querySelectorAll('#checkBoxHome input[type="checkbox"]');
-
-function filterEvents() {
-  let selecionarCategoria = Array.from(filtros)
-    .filter((checkbox) => checkbox.checked)
-    .map((checkbox) => checkbox.value);
-
-  let eventosFiltrados = data.events;
-
-  if (selecionarCategoria.length > 0) {
-    eventosFiltrados = eventosFiltrados.filter((event) =>
-      selecionarCategoria.includes(event.category)
-    );
-  }
-
-  return eventosFiltrados;
-}
-
-function searchEvents(eventosFiltrados) {
-  let barraBusqueda = document.getElementById("buscador").value.toLowerCase();
-  let eventosEncontrados = eventosFiltrados.filter(
-    (evento) =>
-      evento.name.toLowerCase().includes(barraBusqueda) ||
-      evento.description.toLowerCase().includes(barraBusqueda)
-  );
-
-  if (eventosEncontrados.length > 0) {
-    crearEventos(eventosEncontrados);
-  } else {
-    document.getElementById(
-      "contenedor"
-    ).innerHTML = `<p class="fs-3">Ningun evento coincide con su busqueda.</p>`;
-  }
-}
-
-filtros.forEach((checkbox) =>
-  checkbox.addEventListener("change", function () {
-    let eventosFiltrados = filterEvents();
-    searchEvents(eventosFiltrados);
-  })
-);
-
-document.getElementById("buscador").addEventListener("keydown", function () {
-  let eventosFiltrados = filterEvents();
-  searchEvents(eventosFiltrados);
-});
+console.log(eventoRender);
--- assets/js/past.js
+++ assets/js/past.js
@@ -196,23 +196,92 @@ const data = {
 };
 
 let contenedor = document.getElementById("contenedor");
+function crearEventos(a) {
+  contenedor.innerHTML = ``;
+  for (let i = 0; i < a.length; i++) {
+    let fecha = data.events[i].date.slice(0, 4);
+    parseInt(fecha);
+    let hoy = data.currentDate.slice(0, 4);
+    parseInt(hoy);
+    if (fecha < hoy) {
+      let evento = document.createElement("div");
+      evento.classList.add("card", "p-3");
+      evento.innerHTML = `<img src=${a[i].image} class="card-img-top" alt="...">
+  <div class="card-body d-flex flex-column justify-content-between">
+    <h5 class="card-title">${a[i].name}</h5>
+    <p class="card-text">${a[i].description}</p>
+    <div><p class="fw-bold fs-5">Price: $${a[i].price}</p>
+    <a href="./details.html?id=${a[i]._id}" class="btn btn-primary">Details</a></div>
+  </div>`;
 
-for (let i = 0; i < data.events.length; i++) {
-  let fecha = data.events[i].date.slice(0, 4);
-  parseInt(fecha);
-  let hoy = data.currentDate.slice(0, 4); //Podia solo colocar 2023 pero ajam por sia
-  parseInt(hoy);
-  if (fecha < hoy) {
-    let evento = document.createElement("div");
-    evento.classList.add("card", "p-3");
-    evento.innerHTML = `<img
-              src=${data.events[i].image}
-              alt="..."
-            />
-            <div class="card-body d-flex flex-wrap justify-content-around"><h5 class="card-title">${data.events[i].name}</h5>
-              <p class="card-text">${data.events[i].description}</p>
-              <p class="fw-bold">Price: $${data.events[i].price}</p>
-              <a href="./details.html" class="btn btn-danger">Details</a> </div>`;
-    contenedor.appendChild(evento);
+      contenedor.appendChild(evento);
+    }
   }
 }
+crearEventos(data.events);
+
+function crearCheckbox() {
+  let checkboxcreate = [];
+  for (let i = 0; i < data.events.length; i++) {
+    let actual = data.events[i].category;
+    if (checkboxcreate.includes(actual)) {
+    } else {
+      checkboxcreate.push(actual);
+    }
+  }
+  let lugar = document.getElementById("checkBoxHome");
+  let contador = 0;
+  for (let i = 0; i < checkboxcreate.length; i++) {
+    let label = document.createElement("label");
+    contador++;
+    label.innerHTML = `<input type="checkBox" name="option" value="${checkboxcreate[i]}">${checkboxcreate[i]}`;
+    lugar.appendChild(label);
+  }
+}
+crearCheckbox();
+let filtros = document.querySelectorAll('#checkBoxHome input[type="checkbox"]');
+
+function filterEvents() {
+  let selecionarCategoria = Array.from(filtros)
+    .filter((checkbox) => checkbox.checked)
+    .map((checkbox) => checkbox.value);
+
+  let eventosFiltrados = data.events;
+
+  if (selecionarCategoria.length > 0) {
+    eventosFiltrados = eventosFiltrados.filter((event) =>
+      selecionarCategoria.includes(event.category)
+    );
+  }
+
+  return eventosFiltrados;
+}
+
+function searchEvents(eventosFiltrados) {
+  let barraBusqueda = document.getElementById("buscador").value.toLowerCase();
+  let eventosEncontrados = eventosFiltrados.filter(
+    (evento) =>
+      evento.name.toLowerCase().includes(barraBusqueda) ||
+      evento.description.toLowerCase().includes(barraBusqueda)
+  );
+
+  if (eventosEncontrados.length > 0) {
+    crearEventos(eventosEncontrados);
+  } else {
+    document.getElementById(
+      "contenedor"
+    ).innerHTML = `<p class="fs-3">Ningun evento coincide con su busqueda.</p>`;
+  }
+}
+
+filtros.forEach((checkbox) =>
+  checkbox.addEventListener("change", function () {
+    let eventosFiltrados = filterEvents();
+    searchEvents(eventosFiltrados);
+  })
+);
+
+document.getElementById("buscador").addEventListener("keydown", function () {
+  let eventosFiltrados = filterEvents();
+  searchEvents(eventosFiltrados);
+});
